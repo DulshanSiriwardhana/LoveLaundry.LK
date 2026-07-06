9afba27f996a50c80931bf376bc097c4b4267811
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-07-06 17:43:35 +0530

    system:actionbar init

---
+++ client-pub/src/constants/actions.ts
@@ -0,0 +1,18 @@
+export const actions = [
+    {
+        name: "home",
+        path: "/"
+    },
+    {
+        name: "about",
+        path: "/about"
+    },
+    {
+        name: "work",
+        path: "/work"
+    },
+    {
+        name: "join",
+        path: "/join"
+    }
+];
