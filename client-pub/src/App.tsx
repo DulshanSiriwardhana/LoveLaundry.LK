770f0349847e660783196856f894409e2cb724ce
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-07-06 17:59:53 +0530

    system: actionbar #001

--- client-pub/src/App.tsx
+++ client-pub/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
+import ActionBar from './components/actionbar/ActionBar'
 import HomePage from './pages/HomePage'
 
 function App() {
 
   return (
-    <div className='w-full h-screen flex items-center justify-center bg-cyan-100 text-black font-bold text-6xl'>
+    <div className='w-full h-screen flex items-center justify-center text-black font-bold text-6xl'>
       <HomePage/>
+      <ActionBar/>
     </div>
   )
 }
--- client-pub/src/components/actionbar/ActionBar.tsx
+++ client-pub/src/components/actionbar/ActionBar.tsx
@@ -1,6 +1,6 @@
 const ActionBar=()=>{
     return(
-        <div>
+        <div className="max-w-[480px] min-w-[320px] border-2 fixed bottom-20 h-12 rounded-xl w-full border-green-600">
 
         </div>
     )
