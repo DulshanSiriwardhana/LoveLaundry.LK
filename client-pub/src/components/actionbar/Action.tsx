627ab87bf09ec6c8c3fe3bdfe00640a1865e8a7a
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-07-10 14:28:52 +0530

    update: safe

--- client-pub/src/components/actionbar/Action.tsx
+++ client-pub/src/components/actionbar/Action.tsx
@@ -1,9 +1,16 @@
+import { useEffect, useState } from "react";
 import type { ActionType } from "../../types/action";
 
 const Action=({item, isSelected, setSelected}:{item:ActionType, isSelected:boolean, setSelected: (e:ActionType)=>void})=>{
+    const [bgcolor, setBgcolor] = useState<null | string>(null);
+
+    useEffect(()=>{
+        if(isSelected) setBgcolor("green");
+        else setBgcolor(null);
+    },[isSelected]);
 
     return(
-        <div style={{backgroundColor: `${isSelected && "green"}`}} className="border rounded-full w-12 h-12 flex items-center justify-center" onClick={()=>setSelected(item)}>
+        <div style={{backgroundColor: `${bgcolor}`}} className="border rounded-full w-12 h-12 flex items-center justify-center" onClick={()=>setSelected(item)}>
             {<item.icon className="w-6"/>}
         </div>
     )
